22dc569397b99fdde060326e0f97584264383728
obrok <pawel.obrok@gmail.com>
2012-01-27 09:14:50 +0100

    Attach a photo button properly stays hidden on screen rotation

--- src/pl/llp/aircasting/activity/MakeANoteActivity.java
+++ src/pl/llp/aircasting/activity/MakeANoteActivity.java
@@ -1,22 +1,22 @@
 /**
-    AirCasting - Share your Air!
-    Copyright (C) 2011-2012 HabitatMap, Inc.
+ AirCasting - Share your Air!
+ Copyright (C) 2011-2012 HabitatMap, Inc.
 
-    This program is free software: you can redistribute it and/or modify
-    it under the terms of the GNU General Public License as published by
-    the Free Software Foundation, either version 3 of the License, or
-    (at your option) any later version.
+ This program is free software: you can redistribute it and/or modify
+ it under the terms of the GNU General Public License as published by
+ the Free Software Foundation, either version 3 of the License, or
+ (at your option) any later version.
 
-    This program is distributed in the hope that it will be useful,
-    but WITHOUT ANY WARRANTY; without even the implied warranty of
-    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-    GNU General Public License for more details.
+ This program is distributed in the hope that it will be useful,
+ but WITHOUT ANY WARRANTY; without even the implied warranty of
+ MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ GNU General Public License for more details.
 
-    You should have received a copy of the GNU General Public License
-    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ You should have received a copy of the GNU General Public License
+ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
-    You can contact the authors by email at <info@habitatmap.org>
-*/
+ You can contact the authors by email at <info@habitatmap.org>
+ */
 package pl.llp.aircasting.activity;
 
 import android.app.Application;
@@ -89,10 +89,6 @@ public class MakeANoteActivity extends DialogActivity implements View.OnClickLis
         save.setOnClickListener(this);
         share.setOnClickListener(this);
         attachPhoto.setOnClickListener(this);
-
-        if (photoAttached) {
-            attachPhoto.setVisibility(View.GONE);
-        }
     }
 
     @Override
@@ -122,6 +118,10 @@ public class MakeANoteActivity extends DialogActivity implements View.OnClickLis
             }.execute();
 
             initialized = true;
+        }
+
+        if (photoAttached) {
+            attachPhoto.setVisibility(View.GONE);
         }
     }
 
@@ -186,6 +186,7 @@ public class MakeANoteActivity extends DialogActivity implements View.OnClickLis
             Toast.makeText(context, R.string.photo_success, Toast.LENGTH_LONG).show();
 
             attachPhoto.setVisibility(View.GONE);
+            photoAttached = true;
 
             photoHelper.insertIntoGallery(photoPath);
         }
