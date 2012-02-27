0971aaac5672e295ad903e69f91b85e1a8ab93f5
obrok <pawel.obrok@gmail.com>
2012-02-27 09:32:31 +0100

    The location dot is properly centered

--- src/pl/llp/aircasting/view/overlay/LocationOverlay.java
+++ src/pl/llp/aircasting/view/overlay/LocationOverlay.java
@@ -1,22 +1,22 @@
 /**
-    AirCasting - Share your Air!
-    Copyright (C) 2011-2012 HabitatMap, Inc.
+ AirCasting - Share your Air!
+ Copyright (C) 2011-2012 HabitatMap, Inc.
 
-    This program is free software: you can redistribute it and/or modify
-    it under the terms of the GNU General Public License as published by
-    the Free Software Foundation, either version 3 of the License, or
-    (at your option) any later version.
+ This program is free software: you can redistribute it and/or modify
+ it under the terms of the GNU General Public License as published by
+ the Free Software Foundation, either version 3 of the License, or
+ (at your option) any later version.
 
-    This program is distributed in the hope that it will be useful,
-    but WITHOUT ANY WARRANTY; without even the implied warranty of
-    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-    GNU General Public License for more details.
+ This program is distributed in the hope that it will be useful,
+ but WITHOUT ANY WARRANTY; without even the implied warranty of
+ MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ GNU General Public License for more details.
 
-    You should have received a copy of the GNU General Public License
-    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ You should have received a copy of the GNU General Public License
+ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
-    You can contact the authors by email at <info@habitatmap.org>
-*/
+ You can contact the authors by email at <info@habitatmap.org>
+ */
 package pl.llp.aircasting.view.overlay;
 
 import android.graphics.Canvas;
@@ -27,8 +27,9 @@ import com.google.android.maps.GeoPoint;
 import com.google.android.maps.MapView;
 import com.google.android.maps.Overlay;
 import com.google.inject.Inject;
-import pl.llp.aircasting.helper.LocationConversionHelper;
 import pl.llp.aircasting.helper.ResourceHelper;
+
+import static pl.llp.aircasting.helper.LocationConversionHelper.geoPoint;
 
 /**
  * Created by IntelliJ IDEA.
@@ -48,10 +49,14 @@ public class LocationOverlay extends Overlay {
 
         if (location != null) {
             Drawable bullet = resourceHelper.getLocationBullet(value);
-            GeoPoint geoPoint = LocationConversionHelper.geoPoint(location.getLatitude(), location.getLongitude());
+            GeoPoint geoPoint = geoPoint(location);
             Point point = mapView.getProjection().toPixels(geoPoint, null);
 
-            bullet.setBounds(point.x, point.y, point.x + bullet.getIntrinsicWidth(), point.y + bullet.getIntrinsicHeight());
+            int left = point.x - bullet.getIntrinsicWidth() / 2;
+            int top = point.y - bullet.getIntrinsicHeight() / 2;
+            int right = point.x + bullet.getIntrinsicWidth() / 2;
+            int bottom = point.y + bullet.getIntrinsicHeight() / 2;
+            bullet.setBounds(left, top, right, bottom);
             bullet.draw(canvas);
         }
     }
