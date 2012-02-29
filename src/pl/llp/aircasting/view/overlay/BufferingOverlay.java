228d65dab261639d03d1f9f1c753095a8282bb6c
obrok <pawel.obrok@gmail.com>
2012-02-29 12:22:07 +0100

    Make BitmapHolder limited to 2 bitmaps

--- src/pl/llp/aircasting/view/overlay/BufferingOverlay.java
+++ src/pl/llp/aircasting/view/overlay/BufferingOverlay.java
@@ -1,22 +1,22 @@
 /**
-    AirCasting - Share your Air!
-    Copyright (C) 2011-2012 HabitatMap, Inc.
+ AirCasting - Share your Air!
+ Copyright (C) 2011-2012 HabitatMap, Inc.
 
-    This program is free software: you can redistribute it and/or modify
-    it under the terms of the GNU General Public License as published by
-    the Free Software Foundation, either version 3 of the License, or
-    (at your option) any later version.
+ This program is free software: you can redistribute it and/or modify
+ it under the terms of the GNU General Public License as published by
+ the Free Software Foundation, either version 3 of the License, or
+ (at your option) any later version.
 
-    This program is distributed in the hope that it will be useful,
-    but WITHOUT ANY WARRANTY; without even the implied warranty of
-    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-    GNU General Public License for more details.
+ This program is distributed in the hope that it will be useful,
+ but WITHOUT ANY WARRANTY; without even the implied warranty of
+ MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ GNU General Public License for more details.
 
-    You should have received a copy of the GNU General Public License
-    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ You should have received a copy of the GNU General Public License
+ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
-    You can contact the authors by email at <info@habitatmap.org>
-*/
+ You can contact the authors by email at <info@habitatmap.org>
+ */
 package pl.llp.aircasting.view.overlay;
 
 import android.graphics.Bitmap;
@@ -51,7 +51,7 @@ public abstract class BufferingOverlay<UpdateData> extends Overlay {
 
     @Override
     public synchronized void draw(Canvas canvas, MapView mapView, boolean shadow) {
-        if (shadow || bitmap == null || mapView.getZoomLevel() != zoomLevel) return;
+        if (shadow || bitmap == null || bitmap.isRecycled() || mapView.getZoomLevel() != zoomLevel) return;
 
         Point oldCenter = mapView.getProjection().toPixels(mapCenter, null);
         Point newCenter = mapView.getProjection().toPixels(mapView.getMapCenter(), null);
