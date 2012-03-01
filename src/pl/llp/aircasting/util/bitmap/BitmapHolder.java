f8db50b499fc2ae17a75bba4a5e851ff0ed9cb8b
obrok <pawel.obrok@gmail.com>
2012-03-01 09:43:45 +0100

    Revert "Make BitmapHolder limited to 2 bitmaps"
    This reverts commit 228d65dab261639d03d1f9f1c753095a8282bb6c.

--- src/pl/llp/aircasting/util/bitmap/BitmapHolder.java
+++ src/pl/llp/aircasting/util/bitmap/BitmapHolder.java
@@ -1,22 +1,22 @@
 /**
- AirCasting - Share your Air!
- Copyright (C) 2011-2012 HabitatMap, Inc.
+    AirCasting - Share your Air!
+    Copyright (C) 2011-2012 HabitatMap, Inc.
 
- This program is free software: you can redistribute it and/or modify
- it under the terms of the GNU General Public License as published by
- the Free Software Foundation, either version 3 of the License, or
- (at your option) any later version.
+    This program is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
 
- This program is distributed in the hope that it will be useful,
- but WITHOUT ANY WARRANTY; without even the implied warranty of
- MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- GNU General Public License for more details.
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
 
- You should have received a copy of the GNU General Public License
- along with this program.  If not, see <http://www.gnu.org/licenses/>.
+    You should have received a copy of the GNU General Public License
+    along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
- You can contact the authors by email at <info@habitatmap.org>
- */
+    You can contact the authors by email at <info@habitatmap.org>
+*/
 package pl.llp.aircasting.util.bitmap;
 
 import android.graphics.Bitmap;
@@ -24,6 +24,7 @@ import com.google.common.cache.*;
 import com.google.inject.Singleton;
 
 import java.util.concurrent.ExecutionException;
+import java.util.concurrent.TimeUnit;
 
 /**
  * Created by IntelliJ IDEA.
@@ -33,34 +34,21 @@ import java.util.concurrent.ExecutionException;
  */
 @Singleton
 public class BitmapHolder {
-    private static final CacheLoader<BitmapKey, Bitmap> cacheLoader = new CacheLoader<BitmapKey, Bitmap>() {
+    private static final CacheLoader<BitmapKey,Bitmap> cacheLoader = new CacheLoader<BitmapKey, Bitmap>() {
         @Override
         public Bitmap load(BitmapKey key) throws Exception {
             return Bitmap.createBitmap(key.getWidth(), key.getHeight(), Bitmap.Config.ARGB_8888);
         }
     };
 
-    private static final RemovalListener<BitmapKey, Bitmap> listener = new RemovalListener<BitmapKey, Bitmap>() {
-        @Override
-        public void onRemoval(RemovalNotification<BitmapKey, Bitmap> notification) {
-            Bitmap value = notification.getValue();
-            if (value != null) {
-                value.recycle();
-            }
-        }
-    };
-
-    public static final int MAXIMUM_SIZE = 2;
-
     Cache<BitmapKey, Bitmap> cache =
             CacheBuilder.newBuilder()
-                    .removalListener(listener)
-                    .maximumSize(MAXIMUM_SIZE)
+                    .expireAfterAccess(30, TimeUnit.SECONDS)
                     .build(cacheLoader);
 
     public Bitmap getBitmap(int width, int height, int index) {
         try {
-            if (width == 0 || height == 0) return null;
+            if(width == 0 || height == 0) return null;
 
             BitmapKey key = new BitmapKey(width, height, index);
             return cache.get(key);
