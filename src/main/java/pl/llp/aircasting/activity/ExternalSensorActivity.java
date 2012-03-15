44b3a7343c5940ad3d2d594dab718f5d85d93a67
obrok <pawel.obrok@gmail.com>
2012-03-15 12:09:28 +0100

    Added check for bluetooth support

--- src/main/java/pl/llp/aircasting/activity/ExternalSensorActivity.java
+++ src/main/java/pl/llp/aircasting/activity/ExternalSensorActivity.java
@@ -1,13 +1,18 @@
 package pl.llp.aircasting.activity;
 
 import android.bluetooth.BluetoothAdapter;
+import android.content.Context;
 import android.os.Bundle;
+import android.widget.Toast;
 import com.google.inject.Inject;
 import pl.llp.aircasting.Intents;
 import pl.llp.aircasting.R;
 
+import javax.annotation.Nullable;
+
 public class ExternalSensorActivity extends DialogActivity {
-    @Inject BluetoothAdapter bluetoothAdapter;
+    @Nullable @Inject BluetoothAdapter bluetoothAdapter;
+    @Inject Context context;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -18,7 +23,13 @@ public class ExternalSensorActivity extends DialogActivity {
     @Override
     protected void onResume() {
         super.onResume();
-        
+
+        if(bluetoothAdapter == null){
+            Toast.makeText(context, R.string.bluetooth_not_supported, Toast.LENGTH_LONG).show();
+            finish();
+            return;
+        }
+
         if(!bluetoothAdapter.isEnabled()){
             Intents.requestEnableBluetooth(this);
         }
