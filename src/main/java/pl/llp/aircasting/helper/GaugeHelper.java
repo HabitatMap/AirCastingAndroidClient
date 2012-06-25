6697d2136b2482c1c59317faa41682ad49c6533f
Andrzej Grzesik <andrzej.grzesik@gmail.com>
2012-06-25 16:31:25 +0200

    fixes: #305815 Displayed greyed out Avg and Peak when impossible to print anything rational there

--- src/main/java/pl/llp/aircasting/helper/GaugeHelper.java
+++ src/main/java/pl/llp/aircasting/helper/GaugeHelper.java
@@ -15,47 +15,55 @@ import roboguice.inject.InjectResource;
 import static java.lang.String.valueOf;
 
 @Singleton
-public class GaugeHelper {
-    @Inject ResourceHelper resourceHelper;
-    @Inject SettingsHelper settingsHelper;
-    @Inject SessionManager sessionManager;
+public class GaugeHelper
+{
+  @Inject ResourceHelper resourceHelper;
+  @Inject SettingsHelper settingsHelper;
+  @Inject SessionManager sessionManager;
 
-    @InjectResource(R.string.avg_label_template) String avgLabel;
-    @InjectResource(R.string.now_label_template) String nowLabel;
-    @InjectResource(R.string.peak_label_template) String peakLabel;
+  @InjectResource(R.string.avg_label_template) String avgLabel;
+  @InjectResource(R.string.now_label_template) String nowLabel;
+  @InjectResource(R.string.peak_label_template) String peakLabel;
 
-    /**
-     * Update a set of now/avg/peak gauges
-     *
-     * @param sensor The Sensor from which the readings are taken
-     * @param view   The view containing the three gauges
-     */
-    public void updateGauges(Sensor sensor, View view) {
-        updateVisibility(view);
+  /**
+   * Update a set of now/avg/peak gauges
+   *
+   * @param sensor The Sensor from which the readings are taken
+   * @param view   The view containing the three gauges
+   */
+  public void updateGauges(Sensor sensor, View view)
+  {
+    updateVisibility(view);
 
-        int now = (int) sessionManager.getNow(sensor);
-        int avg = (int) sessionManager.getAvg(sensor);
-        int peak = (int) sessionManager.getPeak(sensor);
+    int now = (int) sessionManager.getNow(sensor);
+    updateGauge(view.findViewById(R.id.now_gauge), sensor, MarkerSize.BIG, now);
+    updateLabel(sensor, view.findViewById(R.id.now_label), nowLabel);
 
-        updateGauge(view.findViewById(R.id.avg_gauge), sensor, MarkerSize.SMALL, avg);
-        updateGauge(view.findViewById(R.id.peak_gauge), sensor, MarkerSize.SMALL, peak);
+    boolean hasStats = sessionManager.isSessionStarted() || sessionManager.isSessionSaved();
+    if (hasStats)
+    {
+      int avg = (int) sessionManager.getAvg(sensor);
+      int peak = (int) sessionManager.getPeak(sensor);
 
-        updateLabel(sensor, view.findViewById(R.id.avg_label), avgLabel);
-        updateLabel(sensor, view.findViewById(R.id.peak_label), peakLabel);
+      updateGauge(view.findViewById(R.id.avg_gauge), sensor, MarkerSize.SMALL, avg);
+      updateGauge(view.findViewById(R.id.peak_gauge), sensor, MarkerSize.SMALL, peak);
+
+      updateLabel(sensor, view.findViewById(R.id.avg_label), avgLabel);
+      updateLabel(sensor, view.findViewById(R.id.peak_label), peakLabel);
     }
-
-    private void updateVisibility(View view) {
-        View nowContainer = view.findViewById(R.id.now_container);
-        View avgContainer = view.findViewById(R.id.avg_container);
-        View peakContainer = view.findViewById(R.id.peak_container);
-
-        nowContainer.setVisibility(sessionManager.isSessionSaved() ? View.GONE : View.VISIBLE);
-
-        boolean hasStats = sessionManager.isSessionStarted() || sessionManager.isSessionSaved();
-        int visibility = hasStats ? View.VISIBLE : View.GONE;
-        avgContainer.setVisibility(visibility);
-        peakContainer.setVisibility(visibility);
+    else
+    {
+      displayInactiveGauge(view.findViewById(R.id.avg_gauge), MarkerSize.SMALL);
+      displayInactiveGauge(view.findViewById(R.id.peak_gauge), MarkerSize.SMALL);
     }
+  }
+
+  private void updateVisibility(View view)
+  {
+    View nowContainer = view.findViewById(R.id.now_container);
+
+    nowContainer.setVisibility(sessionManager.isSessionSaved() ? View.GONE : View.VISIBLE);
+  }
 
   private void updateLabel(Sensor sensor, View view, String label)
   {
