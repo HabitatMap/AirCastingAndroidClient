5098a10e4d9a0a00908ab4ac5ec36a4465d568a1
Andrzej Grzesik <andrzej.grzesik@gmail.com>
2012-07-03 11:23:50 +0200

    Naive attempt not to draw on a recycled bitmap

--- src/main/java/pl/llp/aircasting/view/overlay/BufferingOverlay.java
+++ src/main/java/pl/llp/aircasting/view/overlay/BufferingOverlay.java
@@ -20,115 +20,131 @@
 package pl.llp.aircasting.view.overlay;
 
 import pl.llp.aircasting.R;
+import pl.llp.aircasting.util.Constants;
 import pl.llp.aircasting.util.bitmap.BitmapHolder;
 import pl.llp.aircasting.util.map.ProjectionClone;
 
 import android.graphics.Bitmap;
 import android.graphics.Canvas;
 import android.graphics.Point;
+import android.util.Log;
 import com.google.android.maps.GeoPoint;
 import com.google.android.maps.MapView;
 import com.google.android.maps.Overlay;
 import com.google.android.maps.Projection;
 import com.google.inject.Inject;
-import roboguice.inject.InjectResource;
 
-/**
- * Created by IntelliJ IDEA.
- * User: obrok
- * Date: 10/18/11
- * Time: 12:32 PM
- */
-public abstract class BufferingOverlay<UpdateData> extends Overlay {
-    @Inject BitmapHolder bitmapHolder;
-    @InjectResource(R.color.transparent) int transparent;
+public abstract class BufferingOverlay<UpdateData> extends Overlay
+{
+  @Inject BitmapHolder bitmapHolder;
 
-    private Bitmap bitmap;
-    private int bitmapIndex = 0;
+  private Bitmap bitmap;
+  private int bitmapIndex = 0;
 
-    private GeoPoint mapCenter;
-    private int zoomLevel;
-    private Projection projection;
+  private GeoPoint mapCenter;
+  private int zoomLevel;
+  private Projection projection;
 
-    private volatile boolean paused;
+  private volatile boolean paused;
 
-    @Override
-    public synchronized void draw(Canvas canvas, MapView mapView, boolean shadow) {
-        if (shadow || bitmap == null || mapView.getZoomLevel() != zoomLevel) return;
+  @Override
+  public synchronized void draw(Canvas canvas, MapView mapView, boolean shadow)
+  {
+    if (shadow || bitmap == null || mapView.getZoomLevel() != zoomLevel) return;
 
-        Point oldCenter = mapView.getProjection().toPixels(mapCenter, null);
-        Point newCenter = mapView.getProjection().toPixels(mapView.getMapCenter(), null);
+    Projection projection = mapView.getProjection();
+    Point oldCenter = projection.toPixels(mapCenter, null);
+    Point newCenter = projection.toPixels(mapView.getMapCenter(), null);
 
-        int x = oldCenter.x - newCenter.x;
-        int y = oldCenter.y - newCenter.y;
-        canvas.drawBitmap(bitmap, x, y, null);
+    int x = oldCenter.x - newCenter.x;
+    int y = oldCenter.y - newCenter.y;
+    if(bitmap.isRecycled())
+    {
+      String msg = "Trying to draw [" + mapView.getWidth() + ", " + mapView.getHeight() + "] with a recycled bitmap ";
+      Log.e(Constants.TAG, msg);
+      return;
     }
 
-    public void refresh(MapView mapView) {
-        if (shouldDraw())
-            return;
+    canvas.drawBitmap(bitmap, x, y, null);
+  }
 
-        bitmapIndex = 1 - bitmapIndex;
-
-        Canvas canvas = new Canvas();
-
-        int width = mapView.getWidth();
-        int height = mapView.getHeight();
-        Bitmap newBitmap = bitmapHolder.getBitmap(width, height, bitmapIndex);
-
-        if (newBitmap != null) {
-            newBitmap.eraseColor(transparent);
-
-            canvas.setBitmap(newBitmap);
-
-            int newZoomLevel = mapView.getZoomLevel();
-            GeoPoint newMapCenter = mapView.getMapCenter();
-            Projection newProjection = mapView.getProjection();
-            ProjectionClone projectionClone = new ProjectionClone(newProjection, width, height);
-
-            performDraw(canvas, projectionClone);
-
-            synchronized (this) {
-                this.projection = projectionClone;
-                zoomLevel = newZoomLevel;
-                mapCenter = newMapCenter;
-
-                bitmap = newBitmap;
-            }
-        }
+  public void refresh(MapView mapView)
+  {
+    if (drawingIsPaused())
+    {
+      return;
     }
 
-    private boolean shouldDraw() {
-        return paused;
+    bitmapIndex = 1 - bitmapIndex;
+
+    Canvas canvas = new Canvas();
+
+    int width = mapView.getWidth();
+    int height = mapView.getHeight();
+    Bitmap newBitmap = bitmapHolder.getBitmap(width, height, bitmapIndex);
+
+    if (newBitmap != null)
+    {
+      newBitmap.eraseColor(R.color.transparent);
+
+      canvas.setBitmap(newBitmap);
+
+      int newZoomLevel = mapView.getZoomLevel();
+      GeoPoint newMapCenter = mapView.getMapCenter();
+      Projection newProjection = mapView.getProjection();
+      ProjectionClone projectionClone = new ProjectionClone(newProjection, width, height);
+
+      performDraw(canvas, projectionClone);
+
+      synchronized (this)
+      {
+        this.projection = projectionClone;
+        zoomLevel = newZoomLevel;
+        mapCenter = newMapCenter;
+
+        bitmap = newBitmap;
+      }
+    }
+  }
+
+  private boolean drawingIsPaused()
+  {
+    return paused;
+  }
+
+  public void update(UpdateData updateData)
+  {
+    Canvas canvas = null;
+    Projection projectionToUse = null;
+
+    synchronized (this)
+    {
+      if (bitmap != null)
+      {
+        canvas = new Canvas(bitmap);
+        projectionToUse = this.projection;
+      }
     }
 
-    public void update(UpdateData updateData) {
-        Canvas canvas = null;
-        Projection projectionToUse = null;
-
-        synchronized (this) {
-            if (bitmap != null) {
-                canvas = new Canvas(bitmap);
-                projectionToUse = this.projection;
-            }
-        }
-
-        if (canvas != null) {
-            performUpdate(canvas, projectionToUse, updateData);
-        }
+    if (canvas != null)
+    {
+      performUpdate(canvas, projectionToUse, updateData);
     }
+  }
 
-    protected abstract void performDraw(Canvas canvas, Projection projection);
+  protected abstract void performDraw(Canvas canvas, Projection projection);
 
-    protected abstract void performUpdate(Canvas canvas, Projection projection, UpdateData updateData);
+  protected abstract void performUpdate(Canvas canvas, Projection projection, UpdateData updateData);
 
-    protected void stopDrawing(MapView view) {
-        paused = true;
-        bitmap = null;
-        bitmapHolder.release(view.getWidth(), view.getHeight());
-    }
+  protected void stopDrawing(MapView view)
+  {
+    paused = true;
+    bitmap = null;
+    bitmapHolder.release(view.getWidth(), view.getHeight());
+  }
 
-    public void startDrawing() {
-        paused = false;
-    }
+  public void startDrawing()
+  {
+    paused = false;
+  }
 }
