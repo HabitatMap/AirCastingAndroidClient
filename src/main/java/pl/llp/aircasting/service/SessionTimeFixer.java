87d09d58c1ec2105112cd089db32ec2fa1207332
Andrzej Grzesik <andrzej.grzesik@gmail.com>
2012-07-12 13:49:19 +0200

    Session time fixer

--- src/main/java/pl/llp/aircasting/service/SessionTimeFixer.java
+++ src/main/java/pl/llp/aircasting/service/SessionTimeFixer.java
@@ -3,6 +3,7 @@ package pl.llp.aircasting.service;
 import pl.llp.aircasting.model.Measurement;
 import pl.llp.aircasting.model.MeasurementStream;
 import pl.llp.aircasting.model.Session;
+import pl.llp.aircasting.repository.RepositoryException;
 
 import java.util.Date;
 import java.util.List;
@@ -11,11 +12,18 @@ public class SessionTimeFixer
 {
   public void fromUTCtoLocal(Session session)
   {
+    if(needsTimeFix(session))
+    {
+      fixStartEndTimeFromMeasurements(session);
+    }
+
     int minutes = getOffset(session);
     int offset = minutes * 60 * 1000;
 
-    session.setStart(new Date(session.getStart().getTime() + offset));
-    session.setEnd(new Date(session.getEnd().getTime() + offset));
+    Date start = session.getStart();
+    Date end = session.getEnd();
+    session.setStart(new Date(start.getTime() + offset));
+    session.setEnd(new Date(end.getTime() + offset));
 
     List<MeasurementStream> streams = session.getActiveMeasurementStreams();
     for (MeasurementStream stream : streams)
@@ -26,6 +34,11 @@ public class SessionTimeFixer
         measurement.setTime(new Date(measurement.getTime().getTime() + offset));
       }
     }
+  }
+
+  boolean needsTimeFix(Session session)
+  {
+    return session.getStart() == null || session.getEnd() == null;
   }
 
   private int getOffset(Session session)
@@ -40,5 +53,50 @@ public class SessionTimeFixer
       }
     }
     return 0;
+  }
+
+  void fixStartEndTimeFromMeasurements(Session session)
+  {
+    Date start = session.getStart();
+    Date end = session.getEnd();
+
+    for (MeasurementStream stream : session.getMeasurementStreams())
+    {
+      List<Measurement> measurements = stream.getMeasurements();
+      if (measurements.isEmpty())
+      {
+        continue;
+      }
+
+      Measurement first = measurements.get(0);
+      Measurement last = measurements.get(measurements.size() - 1);
+
+      if (start == null)
+      {
+        start = first.getTime();
+      }
+      else
+      {
+        start = start.before(first.getTime()) ? start : first.getTime();
+      }
+
+      if (end == null)
+      {
+        end = last.getTime();
+      }
+      else
+      {
+        end = end.before(last.getTime()) ? last.getTime() : end;
+      }
+    }
+
+    if (start == null || end == null)
+    {
+      String message = "Session [" + session.getId() + "] has incorrect start/end date [" + start + "/" + end + "]";
+      throw new RepositoryException(message);
+    }
+
+    session.setStart(new Date(start.getTime()));
+    session.setEnd(new Date(end.getTime()));
   }
 }
---
+++ src/test/java/pl/llp/aircasting/service/SessionTimeFixerTest.java
@@ -0,0 +1,79 @@
+package pl.llp.aircasting.service;
+
+import pl.llp.aircasting.New;
+import pl.llp.aircasting.model.Measurement;
+import pl.llp.aircasting.model.MeasurementStream;
+import pl.llp.aircasting.model.Session;
+import pl.llp.aircasting.repository.RepositoryException;
+
+import org.junit.Test;
+
+import java.util.Date;
+
+import static junit.framework.Assert.assertEquals;
+import static junit.framework.Assert.assertFalse;
+import static junit.framework.Assert.assertTrue;
+
+/**
+ * Created by ags on 12/07/12 at 12:20
+ */
+public class SessionTimeFixerTest
+{
+  Session session = new Session();
+  SessionTimeFixer fixer = new SessionTimeFixer();
+  private Measurement m2 = New.measurement(2);
+  private Measurement m1 = New.measurement(1);
+  private MeasurementStream stream = new MeasurementStream();
+
+  @Test
+  public void should_needFixForSessionWithoutDates() throws Exception
+  {
+    // then
+    assertTrue(fixer.needsTimeFix(session));
+  }
+
+  @Test
+  public void should_notNeedFixForSessionWithDates() throws Exception
+  {
+    // given
+    session.setStart(new Date());
+    session.setEnd(new Date());
+
+    // then
+    assertFalse(fixer.needsTimeFix(session));
+  }
+
+  @Test
+  public void should_setDatesForSession() throws Exception
+  {
+    // given
+    session.setStart(null);
+    session.setEnd(null);
+    stream.add(m1);
+    stream.add(m2);
+    session.add(stream);
+
+    // when
+    fixer.fixStartEndTimeFromMeasurements(session);
+
+    // then
+    assertEquals(m1.getTime(), session.getStart());
+    assertEquals(m2.getTime(), session.getEnd());
+  }
+
+  @Test(expected = RepositoryException.class)
+  public void should_setComplainAboutNullDatesInSession() throws Exception
+  {
+    // given
+    session.setStart(null);
+    session.setEnd(null);
+    m1.setTime(null);
+    stream.add(m1);
+    session.add(stream);
+
+    // when
+    fixer.fixStartEndTimeFromMeasurements(session);
+
+    // then
+  }
+}
