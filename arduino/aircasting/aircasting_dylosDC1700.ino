a16e6cbae37c16a82121aaa02f55b04a114bccb1
Andrzej Grzesik <andrzej.grzesik@gmail.com>
2012-10-05 16:44:42 +0200

    Added arduino sketches for dylos and shinyei sensors (#438568)

---
+++ arduino/aircasting/aircasting_dylosDC1700.ino
@@ -0,0 +1,46 @@
+//I. Heng and R. Yap
+char line[80];
+int small = 0;
+int large = 0;
+int i =0;
+
+void setup()  {
+  
+  // set the data rate for the SoftwareSerial port
+  Serial1.begin(9600);
+  Serial2.begin(115200);
+  
+  //set up Arduino's Serial
+  Serial.begin(9600);
+  Serial.println("Ready");
+}
+void loop() {
+
+  // listen for new serial coming in:
+  char someChar = Serial1.read();
+
+  if( someChar != -1){
+    //Serial.print(someChar);
+    if (someChar == '\n')
+    {
+      line[i++]=0;
+      sscanf(line,"%d,%d",&small,&large);
+      i=0;
+      Serial.print(small);
+      Serial.print(" : 0.5 Micron  ");
+      Serial.print(large);
+      Serial.println(" : 2.5 Micron");
+
+      Serial2.print(small);
+      Serial2.print(";CityTech56789;DC1700a;Small Particle;SP;0.5 Micron;Micron;0;1000;2000;3000;4000");
+      Serial2.print("\n"); 
+
+      Serial2.print(large);
+      Serial2.print(";CityTech56789;DC1700b;Large Particle;LP;2.5 Micron;Micron;0;1000;2000;3000;4000");
+      Serial2.print("\n");      
+    }
+    else
+      line[i++] = someChar;
+  }
+}
+
---
+++ arduino/aircasting/aircasting_shinyeiPPD42NS.ino
@@ -0,0 +1,40 @@
+//I. Heng and R. Yap
+#include <SoftwareSerial.h>
+unsigned long duration;
+unsigned long starttime;
+unsigned long sampletime_ms = 1000;
+unsigned long lowpulseoccupancy = 0;
+float ratio = 0;
+float concentration = 0;
+
+SoftwareSerial mySerial(2, 3);
+
+void setup() {
+  Serial.begin(9600);
+  mySerial.begin(115200);
+  pinMode(8,INPUT);
+  starttime = millis();
+}
+
+void loop() {
+  duration = pulseIn(8, LOW);
+  lowpulseoccupancy = lowpulseoccupancy+duration;
+  //Serial.print(starttime);
+  //Serial.print(" ");
+  //Serial.println(duration);
+  if ((millis()-starttime) > sampletime_ms)
+  //{
+    ratio = lowpulseoccupancy/(sampletime_ms*10.0);  // Integer percentage 0=>100
+    concentration = 1.1*pow(ratio,3)-3.8*pow(ratio,2)+520*ratio+0.62; // using spec sheet curve
+    Serial.print(lowpulseoccupancy);
+    Serial.print(",");
+    Serial.print(ratio);
+    Serial.print(",");
+    Serial.println(concentration);
+    mySerial.print(concentration);
+    mySerial.print(";CityTech56789;Shinyei PPD42NS;Concentration;Cctn;1 Micron;Micron;0;100;200;300;400");
+    mySerial.print("\n");
+    lowpulseoccupancy = 0;
+    starttime = millis();
+  //}
+}
