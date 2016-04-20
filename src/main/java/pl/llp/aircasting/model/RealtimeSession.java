1bfbb0a8976c73f15d9bfb96bc1d5046042fe246
Marek Ciupak <marek.ciupak@lunarlogic.io>
2016-04-20 20:09:30 +0200

    Include more data in real-time sessions streaming

--- src/main/java/pl/llp/aircasting/model/RealtimeSession.java
+++ src/main/java/pl/llp/aircasting/model/RealtimeSession.java
@@ -6,12 +6,24 @@ import java.util.UUID;
 
 public class RealtimeSession {
   @Expose @SerializedName("session_uuid") private UUID sessionUUID;
-  @Expose @SerializedName("sensor_name") private String sensorName;
   @Expose @SerializedName("measurement") private Measurement measurement;
+  @Expose @SerializedName("sensor_name") private String sensorName;
+  @Expose @SerializedName("sensor_package_name") private String packageName;
+  @Expose @SerializedName("measurement_type") private String measurementType;
+  @Expose @SerializedName("measurement_short_type") private String shortType;
+  @Expose @SerializedName("unit_name") private String unit;
+  @Expose @SerializedName("unit_symbol") private String symbol;
 
   public RealtimeSession(UUID sessionUUID, MeasurementStream stream, Measurement measurement) {
     this.sessionUUID = sessionUUID;
-    this.sensorName = stream.getSensorName();
+
     this.measurement = measurement;
+
+    this.sensorName = stream.getSensorName();
+    this.packageName = stream.getPackageName();
+    this.measurementType = stream.getMeasurementType();
+    this.shortType = stream.getShortType();
+    this.unit = stream.getUnit();
+    this.symbol = stream.getSymbol();
   }
 }
