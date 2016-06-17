284202d8ae5ea1b3b5d1bda2654534ed33c0d97e
Marek Ciupak <marek.ciupak@lunarlogic.io>
2016-06-17 13:22:39 +0200

    Fix migration

--- src/main/java/pl/llp/aircasting/storage/db/SchemaMigrator.java
+++ src/main/java/pl/llp/aircasting/storage/db/SchemaMigrator.java
@@ -117,7 +117,7 @@ public class SchemaMigrator
     if (oldVersion < 35 && newVersion >= 35)
     {
         addColumn(db, SESSION_TABLE_NAME, SESSION_TYPE, Datatype.BOOLEAN);
-        db.execSQL("UPDATE " + SESSION_TABLE_NAME + " SET " + SESSION_TYPE + " = MobileSession" );
+        db.execSQL("UPDATE " + SESSION_TABLE_NAME + " SET " + SESSION_TYPE + " = 'MobileSession' " );
     }
     if (oldVersion < 36 && newVersion >= 36)
     {
