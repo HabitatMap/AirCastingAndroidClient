29855c604615e5ea605e6b12b3133dce9f13d57a
Radosław Wieczorek <radoslaw.wieczorek@lunarlogic.io>
2017-06-23 14:21:01 +0200

    Move dashboard buttons to a fragment

---
+++ src/main/java/pl/llp/aircasting/activity/fragments/DashboardIdleFragment.java
@@ -0,0 +1,44 @@
+package pl.llp.aircasting.activity.fragments;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+import pl.llp.aircasting.R;
+import pl.llp.aircasting.activity.extsens.ExternalSensorActivity;
+
+/**
+ * Created by radek on 23/06/17.
+ */
+public class DashboardIdleFragment extends Fragment implements View.OnClickListener {
+    private View view;
+    private Button microphoneButton;
+    private Button sensorsButton;
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        view = inflater.inflate(R.layout.dashboard_idle_fragment, container, false);
+
+        microphoneButton = (Button) view.findViewById(R.id.dashboard_microphone);
+        sensorsButton = (Button) view.findViewById(R.id.dashboard_sensors);
+
+        microphoneButton.setOnClickListener(this);
+        sensorsButton.setOnClickListener(this);
+
+        return view;
+    }
+
+    @Override
+    public void onClick(View view) {
+        switch(view.getId()) {
+            case R.id.dashboard_microphone:
+                return;
+            case R.id.dashboard_sensors:
+                startActivity(new Intent(getActivity(), ExternalSensorActivity.class));
+                break;
+        }
+    }
+}
