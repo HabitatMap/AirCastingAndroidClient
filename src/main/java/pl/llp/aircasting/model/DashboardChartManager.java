d949963a94f25e0a99f896fb705f495485ab2f5b
Radosław Wieczorek <radoslaw.wieczorek@lunarlogic.io>
2017-07-18 14:28:24 +0200

    ChartManager refactor

--- src/main/java/pl/llp/aircasting/model/DashboardChartManager.java
+++ src/main/java/pl/llp/aircasting/model/DashboardChartManager.java
@@ -1,23 +1,28 @@
 package pl.llp.aircasting.model;
 
 import android.os.Handler;
+import android.util.Log;
 import com.github.mikephil.charting.charts.LineChart;
+import com.github.mikephil.charting.components.Description;
+import com.github.mikephil.charting.components.Legend;
 import com.github.mikephil.charting.components.XAxis;
 import com.github.mikephil.charting.components.YAxis;
 import com.github.mikephil.charting.data.Entry;
 import com.github.mikephil.charting.data.LineData;
 import com.github.mikephil.charting.data.LineDataSet;
+import com.github.mikephil.charting.formatter.IValueFormatter;
+import com.github.mikephil.charting.utils.ViewPortHandler;
 import com.google.common.collect.Lists;
 import com.google.inject.Inject;
 import com.google.inject.Singleton;
+import pl.llp.aircasting.helper.ResourceHelper;
 
-import java.util.ArrayList;
-import java.util.List;
-import java.util.Map;
+import java.text.DecimalFormat;
+import java.text.SimpleDateFormat;
+import java.util.*;
 import java.util.concurrent.CopyOnWriteArrayList;
 
 import static com.google.common.collect.Maps.newHashMap;
-
 
 /**
  * Created by radek on 06/07/17.
@@ -25,21 +30,27 @@ import static com.google.common.collect.Maps.newHashMap;
 @Singleton
 public class DashboardChartManager {
     @Inject SessionManager sessionManager;
+    @Inject ResourceHelper resourceHelper;
+    @Inject SensorManager sensorManager;
 
-    private final static int INTERVAL = 1000 * 60; // 1 minute
+    private final static int ENTRIES_MAX_SIZE = 10;
+    private final static int MOBILE_INTERVAL = 1000 * 60; // 1 minute
+    private final static int FIXED_INTERVAL = 1000 * 60 * 60; // 1 hour
+    private int interval;
     private Map<String, List> averages = newHashMap();
     private Handler handler = new Handler();
     private Runnable updateEntriesTask = new Runnable() {
         @Override
         public void run() {
             updateLiveEntries();
-            handler.postDelayed(updateEntriesTask, INTERVAL);
+            handler.postDelayed(updateEntriesTask, interval);
         }
     };
 
     public void start() {
         averages.clear();
-        handler.postDelayed(updateEntriesTask, INTERVAL);
+        interval = sessionManager.getSession().isFixed() ? FIXED_INTERVAL : MOBILE_INTERVAL;
+        handler.postDelayed(updateEntriesTask, interval);
     }
 
     public void stop() {
@@ -48,21 +59,11 @@ public class DashboardChartManager {
     }
 
     public void drawChart(LineChart chart, Sensor sensor) {
-        YAxis leftAxis = chart.getAxisLeft();
-        YAxis rightAxis = chart.getAxisRight();
-        XAxis xAxis = chart.getXAxis();
-        leftAxis.setEnabled(false);
-        leftAxis.setDrawGridLines(false);
-        leftAxis.setDrawLabels(false);
-        rightAxis.setEnabled(false);
-        rightAxis.setDrawGridLines(false);
-        rightAxis.setDrawLabels(false);
-        xAxis.setEnabled(false);
-        xAxis.setEnabled(false);
-        xAxis.setDrawLabels(true);
-        chart.setNoDataText("No data available yet");
-
         MeasurementStream stream = sessionManager.getMeasurementStream(sensor.getSensorName());
+        String descriptionText = getDescription(stream);
+
+        draw(chart, descriptionText);
+
         if (stream == null) {
             chart.clear();
             return;
@@ -77,26 +78,73 @@ public class DashboardChartManager {
             return;
         }
 
-        LineDataSet dataSet = new LineDataSet(entries, "1 min Avg - " + sensor.getUnit());
-        LineData lineData = new LineData(dataSet);
+        LineData lineData = prepareLineData(entries, sensor.getShortType(), stream);
         chart.setData(lineData);
     }
 
-    public void prepareEntriesForViewing() {
+    private void draw(LineChart chart, String descriptionText) {
+        YAxis leftAxis = chart.getAxisLeft();
+        YAxis rightAxis = chart.getAxisRight();
+        XAxis xAxis = chart.getXAxis();
+        Legend legend = chart.getLegend();
+
+        leftAxis.setEnabled(false);
+        leftAxis.setDrawGridLines(false);
+        leftAxis.setDrawLabels(false);
+        rightAxis.setEnabled(false);
+        rightAxis.setDrawGridLines(false);
+        rightAxis.setDrawLabels(false);
+        xAxis.setEnabled(false);
+        xAxis.setDrawLabels(true);
+        legend.setPosition(Legend.LegendPosition.BELOW_CHART_CENTER);
+
+        Description desc = new Description();
+        desc.setText(descriptionText);
+        desc.setPosition(chart.getWidth(), chart.getHeight() - 10);
+        chart.setDescription(desc);
+        chart.setNoDataText("No data available yet");
+    }
+
+    private String getDescription(MeasurementStream stream) {
+        double time;
+        SimpleDateFormat sdf = new SimpleDateFormat("HH:mm");
+
+        if (sessionManager.isSessionSaved()) {
+            Measurement lastMeasurement = stream.getLastMeasurements(1).get(0);
+            time = lastMeasurement.getTime().getTime();
+        } else {
+            Calendar calendar = Calendar.getInstance();
+            time = calendar.getTime().getTime();
+        }
+
+        return sdf.format(time);
+    }
+
+    private LineData prepareLineData(List entries, String unit, MeasurementStream stream) {
+        LineDataSet dataSet = new LineDataSet(entries, "1 min Avg - " + unit);
+        ArrayList<Integer> colors = prepareDataSetColors(stream, entries);
+
+        dataSet.setCircleColors(colors);
+        dataSet.setDrawCircleHole(false);
+        dataSet.setCircleRadius(7);
+
+        LineData lineData = new LineData(dataSet);
+        lineData.setValueFormatter(new MyValueFormatter());
+        return lineData;
+    }
+
+    private void prepareEntriesForViewing() {
         List<MeasurementStream> streams = (List) sessionManager.getMeasurementStreams();
 
         for (MeasurementStream stream : streams) {
             double xValue = 0;
+            double measurementsInPeriod = 60 / stream.getFrequency();
             String sensorName = stream.getSensorName();
             List entries = new CopyOnWriteArrayList();
             List<Measurement> measurements = stream.getMeasurementsForPeriod(10);
-            double measurementsInPeriod = 60 / stream.getFrequency();
             List<List<Measurement>> periodData = new ArrayList(Lists.partition(measurements, (int) measurementsInPeriod));
-            List<Measurement> lastPeriodData = periodData.get(periodData.size() - 1);
 
-            if (lastPeriodData.size() < measurementsInPeriod) {
-                periodData.remove(periodData.size() - 1);
-            }
+            trimIncompletePeriodData(periodData, measurementsInPeriod);
 
             if (periodData.size() > 0) {
                 for (List<Measurement> batch : periodData) {
@@ -111,6 +159,14 @@ public class DashboardChartManager {
             }
 
             averages.put(sensorName, entries);
+        }
+    }
+
+    private void trimIncompletePeriodData(List<List<Measurement>> periodData, double measurementsInPeriod) {
+        List<Measurement> lastPeriodData = periodData.get(periodData.size() - 1);
+
+        if (lastPeriodData.size() < measurementsInPeriod) {
+            periodData.remove(periodData.size() - 1);
         }
     }
 
@@ -135,12 +191,23 @@ public class DashboardChartManager {
             }
 
             entries.add(new Entry((float) xValue, (float) yValue));
-            if (entries.size() > 10) {
+            if (entries.size() > ENTRIES_MAX_SIZE) {
                 entries.remove(0);
             }
 
             averages.put(sensorName, entries);
         }
+    }
+
+    private ArrayList<Integer> prepareDataSetColors(MeasurementStream stream, List<Entry> entries) {
+        ArrayList colors = new ArrayList<Integer>();
+
+        for (Entry entry : entries) {
+            Sensor sensor = sensorManager.getSensorByName(stream.getSensorName());
+            colors.add(resourceHelper.getColorAbsolute(sensor, entry.getY()));
+        }
+
+        return colors;
     }
 
     private List<Entry> getEntriesForStream(MeasurementStream stream) {
@@ -154,6 +221,20 @@ public class DashboardChartManager {
             sum += measurement.getValue();
         }
 
-        return (sum / measurements.size());
+        return Math.round((sum / measurements.size()));
+    }
+
+    public class MyValueFormatter implements IValueFormatter {
+
+        private DecimalFormat mFormat;
+
+        public MyValueFormatter() {
+            mFormat = new DecimalFormat("###,###,###");
+        }
+
+        @Override
+        public String getFormattedValue(float value, Entry entry, int dataSetIndex, ViewPortHandler viewPortHandler) {
+            return mFormat.format(value);
+        }
     }
 }
