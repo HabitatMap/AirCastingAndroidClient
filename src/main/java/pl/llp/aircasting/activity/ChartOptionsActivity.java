09c797b35ba1e2c05974b247d9bfc4a6234fadbf
Radosław Wieczorek <radoslaw.wieczorek@lunarlogic.io>
2017-08-06 17:20:31 +0200

    Open stream options on click

---
+++ src/main/java/pl/llp/aircasting/activity/ChartOptionsActivity.java
@@ -0,0 +1,42 @@
+package pl.llp.aircasting.activity;
+
+import android.content.Context;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import com.google.inject.Inject;
+import pl.llp.aircasting.R;
+import roboguice.inject.InjectView;
+
+/**
+ * Created by radek on 04/08/17.
+ */
+public class ChartOptionsActivity extends DialogActivity implements View.OnClickListener {
+    @Inject Context context;
+    @InjectView(R.id.graph_button) Button graphButton;
+    @InjectView(R.id.map_button) Button mapButton;
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.dashboard_chart_options);
+
+        graphButton.setOnClickListener(this);
+        mapButton.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View view) {
+        switch(view.getId()) {
+            case R.id.graph_button:
+                startActivity(new Intent(context, GraphActivity.class));
+                finish();
+                break;
+            case R.id.map_button:
+                startActivity(new Intent(context, AirCastingMapActivity.class));
+                finish();
+                break;
+        }
+    }
+}
