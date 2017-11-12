956521cbbb6e545e29b0cbf731079b05750bf4f0
Radosław Wieczorek <radoslaw.wieczorek@lunarlogic.io>
2017-11-12 18:42:25 +0100

    Refactor SessionState from SessionDataFactory

--- src/main/java/pl/llp/aircasting/helper/SessionState.java
+++ src/main/java/pl/llp/aircasting/helper/SessionState.java
@@ -1,7 +1,31 @@
 package pl.llp.aircasting.helper;
 
+import com.google.inject.Inject;
+import org.jetbrains.annotations.NotNull;
+import pl.llp.aircasting.activity.ApplicationState;
+import pl.llp.aircasting.model.CurrentSessionManager;
+import pl.llp.aircasting.util.Constants;
+
 /**
  * Created by radek on 08/11/17.
  */
 public class SessionState {
+    @Inject ApplicationState state;
+    @Inject CurrentSessionManager currentSessionManager;
+
+    public boolean isSessionCurrent(@NotNull long sessionId) {
+        return sessionId == Constants.CURRENT_SESSION_FAKE_ID;
+    }
+
+    public boolean isSessionRecording(@NotNull long sessionId) {
+        return sessionId == Constants.CURRENT_SESSION_FAKE_ID && state.recording().isRecording();
+    }
+
+    public boolean isSessionBeingViewed(@NotNull long sessionId) {
+        return sessionId != Constants.CURRENT_SESSION_FAKE_ID;
+    }
+
+    public boolean isCurrentSessionIdle() {
+        return currentSessionManager.isSessionIdle();
+    }
 }
