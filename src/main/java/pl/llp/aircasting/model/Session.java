efe201620c2178b0ad0942dd05bc5316f3d11f81
Radosław Wieczorek <radoslaw.wieczorek@lunarlogic.io>
2017-11-16 12:46:17 +0100

    Fix formatting in Session model

--- src/main/java/pl/llp/aircasting/model/Session.java
+++ src/main/java/pl/llp/aircasting/model/Session.java
@@ -1,22 +1,22 @@
 /**
-    AirCasting - Share your Air!
-    Copyright (C) 2011-2012 HabitatMap, Inc.
+ AirCasting - Share your Air!
+ Copyright (C) 2011-2012 HabitatMap, Inc.
 
-    This program is free software: you can redistribute it and/or modify
-    it under the terms of the GNU General Public License as published by
-    the Free Software Foundation, either version 3 of the License, or
-    (at your option) any later version.
+ This program is free software: you can redistribute it and/or modify
+ it under the terms of the GNU General Public License as published by
+ the Free Software Foundation, either version 3 of the License, or
+ (at your option) any later version.
 
-    This program is distributed in the hope that it will be useful,
-    but WITHOUT ANY WARRANTY; without even the implied warranty of
-    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-    GNU General Public License for more details.
+ This program is distributed in the hope that it will be useful,
+ but WITHOUT ANY WARRANTY; without even the implied warranty of
+ MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ GNU General Public License for more details.
 
-    You should have received a copy of the GNU General Public License
-    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ You should have received a copy of the GNU General Public License
+ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
-    You can contact the authors by email at <info@habitatmap.org>
-*/
+ You can contact the authors by email at <info@habitatmap.org>
+ */
 package pl.llp.aircasting.model;
 
 import com.google.common.base.Predicate;
@@ -38,57 +38,49 @@ import static com.google.common.collect.Iterables.filter;
 import static com.google.common.collect.Lists.newArrayList;
 import static com.google.common.collect.Maps.newHashMap;
 
-public class Session implements Serializable
-{
-  @Expose private UUID uuid = UUID.randomUUID();
+public class Session implements Serializable {
+    @Expose @SerializedName("streams") Map<String, MeasurementStream> streams = newHashMap();
+    @Expose @SerializedName("tag_list") private String tags;
+    @Expose @SerializedName("os_version") private String osVersion;
+    @Expose @SerializedName("phone_model") private String phoneModel;
+    @Expose @SerializedName("offset_60_db") private int offset60DB;
+    @Expose @SerializedName("deleted") private boolean markedForRemoval;
+    @Expose @SerializedName("start_time") private Date start = new Date();
+    @Expose @SerializedName("end_time") private Date end;
 
-  @Expose @SerializedName("streams") Map<String, MeasurementStream> streams = newHashMap();
+    @Expose private List<Note> notes = newArrayList();
+    @Expose private UUID uuid = UUID.randomUUID();
 
-  @Expose private List<Note> notes = newArrayList();
+    @Expose private String title;
+    @Expose private String description;
+    @Expose private String location;
+    @Expose private String type = "MobileSession";
+    @Expose private int calibration;
+    @Expose private boolean contribute;
+    @Expose private int drawable = R.drawable.session_mobile_icon;
+    @Expose private boolean isIndoor;
+    @Expose private double latitude;
+    @Expose private double longitude;
 
-  @Expose private String title;
-  @Expose @SerializedName("tag_list") private String tags;
-  @Expose private String description;
-  @Expose private int calibration;
-  @Expose private boolean contribute;
+    private volatile Long id = null;
+    private boolean submittedForRemoval = false;
+    private boolean locationless = false;
 
-  @Expose @SerializedName("os_version") private String osVersion;
-  @Expose @SerializedName("phone_model") private String phoneModel;
-  @Expose @SerializedName("offset_60_db") private int offset60DB;
+    public Session() {}
 
-  @Expose private String location;
-  @Expose @SerializedName("deleted") private boolean markedForRemoval;
+    public Session(boolean isFixed) {
+        this.setFixed(isFixed);
 
-  @Expose @SerializedName("start_time") private Date start = new Date();
-  @Expose @SerializedName("end_time") private Date end;
+        if (this.isFixed()) {
+            setContribute(true);
+        }
+    }
 
-  @Expose private String type = "MobileSession";
-  @Expose private int drawable = R.drawable.session_mobile_icon;
-  @Expose private boolean isIndoor;
+    public void add(MeasurementStream stream) {
+        streams.put(stream.getSensorName(), stream);
+    }
 
-  @Expose private double latitude;
-  @Expose private double longitude;
-
-  private volatile Long id = null;
-  private boolean submittedForRemoval = false;
-
-  private boolean locationless = false;
-
-  public Session() {}
-
-  public Session(boolean isFixed) {
-    this.setFixed(isFixed);
-
-    if(this.isFixed())
-      setContribute(true);
-  }
-
-  public void add(MeasurementStream stream)
-  {
-    streams.put(stream.getSensorName(), stream);
-  }
-
-  public void setTitle(String text) {
+    public void setTitle(String text) {
         title = text;
     }
 
@@ -112,14 +104,12 @@ public class Session implements Serializable
         return description;
     }
 
-  public Date getEnd()
-  {
-    return end;
-  }
+    public Date getEnd() {
+        return end;
+    }
 
-    public Date getStart()
-    {
-      return start;
+    public Date getStart() {
+        return start;
     }
 
     public void setEnd(Date end) {
@@ -130,153 +120,124 @@ public class Session implements Serializable
         this.start = start;
     }
 
-  public void setId(long id)
-  {
-    this.id = id;
-  }
-
-  public Long getId()
-  {
-    return id;
-  }
-
-  public List<Note> getNotes()
-  {
-    return notes;
-  }
-
-  public void add(Note note)
-  {
-    notes.add(note);
-    note.setNumber(notes.size());
-  }
-
-  public void addAll(Collection<Note> notesToAdd)
-  {
-    notes.addAll(notesToAdd);
-  }
-
-  public void setUuid(UUID uuid)
-  {
-    this.uuid = uuid;
-  }
-
-  public UUID getUUID()
-  {
-    return uuid;
-  }
-
-  public void setLocation(String location)
-  {
-    this.location = location;
-  }
-
-  public String getLocation()
-  {
-    return location;
-  }
-
-  public int getCalibration()
-  {
-    return calibration;
-  }
-
-  public void setCalibration(int calibration)
-  {
-    this.calibration = calibration;
-  }
-
-  public void setContribute(boolean contribute)
-  {
-    this.contribute = contribute;
-  }
-
-  public boolean getContribute()
-  {
-    return contribute;
-  }
-
-  public String getOSVersion()
-  {
-    return osVersion;
-  }
-
-  public String getPhoneModel()
-  {
-    return phoneModel;
-  }
-
-  public void setOsVersion(String osVersion)
-  {
-    this.osVersion = osVersion;
-  }
-
-  public void setPhoneModel(String phoneModel)
-  {
-    this.phoneModel = phoneModel;
-  }
-
-  public int getOffset60DB()
-  {
-    return offset60DB;
-  }
-
-  public void setOffset60DB(int offset60DB)
-  {
-    this.offset60DB = offset60DB;
-  }
-
-  public boolean isMarkedForRemoval()
-  {
-    return markedForRemoval;
-  }
-
-  public void setMarkedForRemoval(boolean markedForRemoval)
-  {
-    this.markedForRemoval = markedForRemoval;
-  }
-
-  public void deleteNote(Note note)
-  {
-    notes.remove(note);
-    reorderNotes();
-  }
-
-  private void reorderNotes()
-  {
-    for (int i = 0; i < notes.size(); i++)
-    {
-      Note note1 = notes.get(i);
-      note1.setNumber(i + 1);
+    public void setId(long id) {
+        this.id = id;
     }
-  }
 
-  public void setSubmittedForRemoval(boolean submittedForRemoval)
-  {
-    this.submittedForRemoval = submittedForRemoval;
-  }
+    public Long getId() {
+        return id;
+    }
 
-  public boolean isSubmittedForRemoval()
-  {
-    return submittedForRemoval;
-  }
+    public List<Note> getNotes() {
+        return notes;
+    }
 
-  public Collection<MeasurementStream> getMeasurementStreams()
-  {
-    return streams.values();
-  }
+    public void add(Note note) {
+        notes.add(note);
+        note.setNumber(notes.size());
+    }
 
-  public MeasurementStream getStream(String sensorName)
-  {
-    return streams.get(sensorName);
-  }
+    public void addAll(Collection<Note> notesToAdd) {
+        notes.addAll(notesToAdd);
+    }
 
-  public boolean hasStream(String sensorName) {
+    public void setUuid(UUID uuid) {
+        this.uuid = uuid;
+    }
+
+    public UUID getUUID() {
+        return uuid;
+    }
+
+    public void setLocation(String location) {
+        this.location = location;
+    }
+
+    public String getLocation() {
+        return location;
+    }
+
+    public int getCalibration() {
+        return calibration;
+    }
+
+    public void setCalibration(int calibration) {
+        this.calibration = calibration;
+    }
+
+    public void setContribute(boolean contribute) {
+        this.contribute = contribute;
+    }
+
+    public boolean getContribute() {
+        return contribute;
+    }
+
+    public String getOSVersion() {
+        return osVersion;
+    }
+
+    public String getPhoneModel() {
+        return phoneModel;
+    }
+
+    public void setOsVersion(String osVersion) {
+        this.osVersion = osVersion;
+    }
+
+    public void setPhoneModel(String phoneModel) {
+        this.phoneModel = phoneModel;
+    }
+
+    public int getOffset60DB() {
+        return offset60DB;
+    }
+
+    public void setOffset60DB(int offset60DB) {
+        this.offset60DB = offset60DB;
+    }
+
+    public boolean isMarkedForRemoval() {
+        return markedForRemoval;
+    }
+
+    public void setMarkedForRemoval(boolean markedForRemoval) {
+        this.markedForRemoval = markedForRemoval;
+    }
+
+    public void deleteNote(Note note) {
+        notes.remove(note);
+        reorderNotes();
+    }
+
+    private void reorderNotes() {
+        for (int i = 0; i < notes.size(); i++) {
+            Note note1 = notes.get(i);
+            note1.setNumber(i + 1);
+        }
+    }
+
+    public void setSubmittedForRemoval(boolean submittedForRemoval) {
+        this.submittedForRemoval = submittedForRemoval;
+    }
+
+    public boolean isSubmittedForRemoval() {
+        return submittedForRemoval;
+    }
+
+    public Collection<MeasurementStream> getMeasurementStreams() {
+        return streams.values();
+    }
+
+    public MeasurementStream getStream(String sensorName) {
+        return streams.get(sensorName);
+    }
+
+    public boolean hasStream(String sensorName) {
         return streams.containsKey(sensorName);
     }
 
-    public boolean isEmpty() {
-        return all(streams.values(), new Predicate<MeasurementStream>() {
+    public boolean isEmpty() { return all(streams.values(), new Predicate<MeasurementStream>() {
             @Override
             public boolean apply(@Nullable MeasurementStream input) {
                 return input.isEmpty();
@@ -296,95 +257,86 @@ public class Session implements Serializable
         return noStreams || noMeasurements;
     }
 
-  public void removeStream(MeasurementStream stream)
-  {
-    String sensorName = stream.getSensorName();
-    streams.remove(sensorName);
-  }
-
-  public List<MeasurementStream> getActiveMeasurementStreams()
-  {
-    return newArrayList(filter(getMeasurementStreams(), new Predicate<MeasurementStream>()
-    {
-      @Override
-      public boolean apply(@Nullable MeasurementStream stream)
-      {
-        return stream != null && !stream.isMarkedForRemoval() && stream.isVisible();
-      }
-    }));
-  }
-
-  @Override
-  public String toString()
-  {
-    return "Session{" +
-        "id=" + id +
-        ", title='" + title + '\'' +
-        ", markedForRemoval=" + markedForRemoval +
-        ", submittedForRemoval=" + submittedForRemoval +
-        '}';
-  }
-
-  public boolean isLocationless()
-  {
-    return locationless;
-  }
-
-  public void setLocationless(boolean locationless)
-  {
-    this.locationless = locationless;
-  }
-
-  public boolean isFixed() {
-    if (this.type.equals("FixedSession")) {
-      return true;
+    public void removeStream(MeasurementStream stream) {
+        String sensorName = stream.getSensorName();
+        streams.remove(sensorName);
     }
-    else {
-      return false;
+
+    public List<MeasurementStream> getActiveMeasurementStreams() {
+        return newArrayList(filter(getMeasurementStreams(), new Predicate<MeasurementStream>() {
+            @Override
+            public boolean apply(@Nullable MeasurementStream stream) {
+                return stream != null && !stream.isMarkedForRemoval() && stream.isVisible();
+            }
+        }));
     }
-  }
 
-  public void setFixed(boolean isFixed) {
-    if (isFixed) {
-      this.type = "FixedSession";
-      this.drawable = R.drawable.session_fixed_icon;
+    @Override
+    public String toString() {
+        return "Session{" +
+                "id=" + id +
+                ", title='" + title + '\'' +
+                ", markedForRemoval=" + markedForRemoval +
+                ", submittedForRemoval=" + submittedForRemoval +
+                '}';
     }
-    else {
-      this.type = "MobileSession";
+
+    public boolean isLocationless() {
+        return locationless;
     }
-  }
 
-  public String getType() {
-    return this.type;
-  }
+    public void setLocationless(boolean locationless) {
+        this.locationless = locationless;
+    }
 
-  public int getDrawable() { return this.drawable; }
+    public boolean isFixed() {
+        if (this.type.equals("FixedSession")) {
+            return true;
+        } else {
+            return false;
+        }
+    }
 
-  public void setType(String type) {
-    this.type = type;
-  }
+    public void setFixed(boolean isFixed) {
+        if (isFixed) {
+            this.type = "FixedSession";
+            this.drawable = R.drawable.session_fixed_icon;
+        } else {
+            this.type = "MobileSession";
+        }
+    }
 
-  public boolean isIndoor() {
-    return isIndoor;
-  }
+    public String getType() {
+        return this.type;
+    }
 
-  public void setIndoor(boolean isIndoor) {
-    this.isIndoor = isIndoor;
-  }
+    public int getDrawable() { return this.drawable; }
 
-  public double getLatitude() {
-    return latitude;
-  }
+    public void setType(String type) {
+        this.type = type;
+    }
 
-  public void setLatitude(Double latitude) {
-    this.latitude = latitude;
-  }
+    public boolean isIndoor() {
+        return isIndoor;
+    }
 
-  public double getLongitude() {
-    return longitude;
-  }
+    public void setIndoor(boolean isIndoor) {
+        this.isIndoor = isIndoor;
+    }
 
-  public void setLongitude(Double longitude) {
-    this.longitude = longitude;
-  }
+    public double getLatitude() {
+        return latitude;
+    }
+
+    public void setLatitude(Double latitude) {
+        this.latitude = latitude;
+    }
+
+    public double getLongitude() {
+        return longitude;
+    }
+
+    public void setLongitude(Double longitude) {
+        this.longitude = longitude;
+    }
 }
