629559248745ee2fad309b0b900ee24fbd3e3828
Radosław Wieczorek <radoslaw.wieczorek@lunarlogic.io>
2017-12-19 15:13:30 +0100

    Move fixed session creation to async task

--- src/main/java/pl/llp/aircasting/sync/FixedSessionUploader.java
+++ src/main/java/pl/llp/aircasting/sync/FixedSessionUploader.java
@@ -1,27 +1,28 @@
 /**
- AirCasting - Share your Air!
- Copyright (C) 2011-2012 HabitatMap, Inc.
-
- This program is free software: you can redistribute it and/or modify
- it under the terms of the GNU General Public License as published by
- the Free Software Foundation, either version 3 of the License, or
- (at your option) any later version.
-
- This program is distributed in the hope that it will be useful,
- but WITHOUT ANY WARRANTY; without even the implied warranty of
- MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- GNU General Public License for more details.
-
- You should have received a copy of the GNU General Public License
- along with this program.  If not, see <http://www.gnu.org/licenses/>.
-
- You can contact the authors by email at <info@habitatmap.org>
+ * AirCasting - Share your Air!
+ * Copyright (C) 2011-2012 HabitatMap, Inc.
+ * <p>
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ * <p>
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ * <p>
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ * <p>
+ * You can contact the authors by email at <info@habitatmap.org>
  */
 package pl.llp.aircasting.sync;
 
 import android.content.Context;
 import android.net.ConnectivityManager;
 import android.net.NetworkInfo;
+import android.os.AsyncTask;
 import android.os.StrictMode;
 import android.util.Log;
 import android.widget.Toast;
@@ -42,78 +43,64 @@ import roboguice.inject.InjectResource;
 import com.google.common.eventbus.Subscribe;
 
 @Singleton
-public class FixedSessionUploader
-{
-  @Inject ConnectivityManager connectivityManager;
-  @Inject SettingsHelper settingsHelper;
-  @Inject Context context;
-  @Inject
-  FixedSessionDriver fixedSessionDriver;
-  @Inject EventBus eventBus;
+public class FixedSessionUploader {
+    @Inject ConnectivityManager connectivityManager;
+    @Inject SettingsHelper settingsHelper;
+    @Inject Context context;
+    @Inject FixedSessionDriver fixedSessionDriver;
+    @Inject EventBus eventBus;
 
-  @InjectResource(R.string.fixed_session_creation_failed) String fixed_session_creation_failed;
+    @InjectResource(R.string.fixed_session_creation_failed)
+    String fixed_session_creation_failed;
 
-  @Inject
-  public void init() {
-    eventBus.register(this);
-  }
+    public boolean create(final Session session) {
+        try {
+            if (canUpload()) {
+                new AsyncTask<Void, Void, Boolean>() {
+                    @Override
+                    protected Boolean doInBackground(Void... voids) {
+                        performCreateSession(session);
 
-  public boolean create(Session session) {
-    try {
-      if (canUpload()) {
-//        enableStrictMode();
-        performCreateSession(session);
-        return (true);
-      } else {
-        Toast.makeText(context, fixed_session_creation_failed, Toast.LENGTH_LONG).show();
-        return (false);
-      }
-    } catch (SessionSyncException exception)
-    {
-      Toast.makeText(context, fixed_session_creation_failed, Toast.LENGTH_LONG).show();
-      return (false);
+                        return true;
+                    }
+                }.execute();
+                return (true);
+            } else {
+                Toast.makeText(context, fixed_session_creation_failed, Toast.LENGTH_LONG).show();
+                return (false);
+            }
+        } catch (SessionSyncException exception) {
+            Toast.makeText(context, fixed_session_creation_failed, Toast.LENGTH_LONG).show();
+            return (false);
+        }
     }
-  }
 
-  // This is BAD but easy workaround to the fixed sessions creation error, use only for dev purposes.
-  // It enables doing network operations in the main thread of the app. It should be done in a background task.
-  // TODO:
-  // Fix this later, unless we don't need streaming anymore.
-  public void enableStrictMode()
-  {
-    StrictMode.ThreadPolicy policy = new StrictMode.ThreadPolicy.Builder().permitAll().build();
+    private void performCreateSession(Session session) throws SessionSyncException {
+        HttpResult<CreateSessionResponse> result = fixedSessionDriver.create(session);
 
-    StrictMode.setThreadPolicy(policy);
-  }
-
-  private void performCreateSession(Session session) throws SessionSyncException
-  {
-    HttpResult<CreateSessionResponse> result = fixedSessionDriver.create(session);
-
-    Status status = result.getStatus();
-    if(status == Status.ERROR || status == Status.FAILURE)
-      throw new SessionSyncException("Session creation failed");
-    CreateSessionResponse createSessionResponse = result.getContent();
-  }
-
-  @Subscribe
-  public void onEvent(FixedSessionsMeasurementEvent event) {
-    if (canUpload()) {
-      performCreateMeasurement(event.getFixedSessionsMeasurement());
+        Status status = result.getStatus();
+        if (status == Status.ERROR || status == Status.FAILURE)
+            throw new SessionSyncException("Session creation failed");
+        CreateSessionResponse createSessionResponse = result.getContent();
     }
-  }
 
-  private void performCreateMeasurement(FixedSessionsMeasurement fixedSessionsMeasurement)
-  {
-    HttpResult<CreateFixedSessionsMeasurementResponse> result = fixedSessionDriver.create_measurement(fixedSessionsMeasurement);
-  }
+    @Subscribe
+    public void onEvent(FixedSessionsMeasurementEvent event) {
+        if (canUpload()) {
+            performCreateMeasurement(event.getFixedSessionsMeasurement());
+        }
+    }
 
-  private boolean canUpload() {
-    NetworkInfo networkInfo = connectivityManager.getActiveNetworkInfo();
+    private void performCreateMeasurement(FixedSessionsMeasurement fixedSessionsMeasurement) {
+        HttpResult<CreateFixedSessionsMeasurementResponse> result = fixedSessionDriver.create_measurement(fixedSessionsMeasurement);
+    }
 
-    return networkInfo != null
-        && networkInfo.isConnected()
-        && (!settingsHelper.isSyncOnlyWifi() || networkInfo.getType() == ConnectivityManager.TYPE_WIFI)
-        && settingsHelper.hasCredentials();
-  }
+    private boolean canUpload() {
+        NetworkInfo networkInfo = connectivityManager.getActiveNetworkInfo();
+
+        return networkInfo != null
+                && networkInfo.isConnected()
+                && (!settingsHelper.isSyncOnlyWifi() || networkInfo.getType() == ConnectivityManager.TYPE_WIFI)
+                && settingsHelper.hasCredentials();
+    }
 }
