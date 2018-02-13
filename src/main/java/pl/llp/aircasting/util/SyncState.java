4f02ed4f76f098ddacb078b9a756d48a86c393d9
Radosław Wieczorek <radoslaw.wieczorek@lunarlogic.io>
2018-02-13 10:47:52 +0100

    Show syncing spinner in sessions activity

--- src/main/java/pl/llp/aircasting/util/SyncState.java
+++ src/main/java/pl/llp/aircasting/util/SyncState.java
@@ -1,36 +1,32 @@
 package pl.llp.aircasting.util;
 
+import pl.llp.aircasting.android.Logger;
 import pl.llp.aircasting.helper.SettingsHelper;
 
 import com.google.inject.Inject;
 import com.google.inject.Singleton;
 
 @Singleton
-public class SyncState
-{
-  @Inject SettingsHelper settings;
+public class SyncState {
+    private States state = States.NOT_IN_PROGRESS;
 
-  private States state = States.NOT_IN_PROGRESS;
+    public void markSyncComplete() {
+        Logger.w("sync complete");
+        state = States.NOT_IN_PROGRESS;
+    }
 
-  public void markSyncComplete()
-  {
-    state = States.NOT_IN_PROGRESS;
-  }
+    public void startSync() {
+        Logger.w("sync started");
+        state = States.IN_PROGRESS;
+    }
 
-  public void startSync()
-  {
-    state = States.IN_PROGRESS;
-  }
+    public synchronized boolean isInProgress() {
+        return state == States.IN_PROGRESS;
+    }
 
-  public synchronized boolean isInProgress()
-  {
-    return state == States.IN_PROGRESS;
-  }
-
-  public enum States
-  {
-    IN_PROGRESS,
-    SYNC_POSSIBLE,
-    NOT_IN_PROGRESS
-  }
+    public enum States {
+        IN_PROGRESS,
+        SYNC_POSSIBLE,
+        NOT_IN_PROGRESS
+    }
 }
