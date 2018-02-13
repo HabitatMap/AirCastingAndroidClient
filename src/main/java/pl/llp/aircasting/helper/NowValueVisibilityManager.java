01fb3d13c60c6f33b22bf2f25e5216944e5b1d35
Radosław Wieczorek <radoslaw.wieczorek@lunarlogic.io>
2018-02-13 11:04:22 +0100

    Formatting & method rename

--- src/main/java/pl/llp/aircasting/helper/NowValueVisibilityManager.java
+++ src/main/java/pl/llp/aircasting/helper/NowValueVisibilityManager.java
@@ -6,14 +6,12 @@ import com.google.inject.Inject;
 /**
  * Created by ags on 02/04/2013 at 20:52
  */
-public class NowValueVisibilityManager
-{
-  @Inject VisibleSession visibleSession;
+public class NowValueVisibilityManager {
+    @Inject VisibleSession visibleSession;
 
-  int getVisibility()
-  {
-    boolean shouldDisplay = visibleSession.isCurrentSessionVisible();
-    int visibility = shouldDisplay ? View.VISIBLE : View.GONE;
-    return visibility;
-  }
+    int getVisibility() {
+        boolean shouldDisplay = visibleSession.isCurrentSessionVisible();
+        int visibility = shouldDisplay ? View.VISIBLE : View.GONE;
+        return visibility;
+    }
 }
