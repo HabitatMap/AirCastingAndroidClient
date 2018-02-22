2b63c8eb9a8ceabf269465b88b28b694aa586eef
Radosław Wieczorek <radoslaw.wieczorek@lunarlogic.io>
2018-02-22 14:47:28 +0100

    Sessions styling tweaks & formattting

--- src/main/java/pl/llp/aircasting/activity/adapter/SessionAdapter.java
+++ src/main/java/pl/llp/aircasting/activity/adapter/SessionAdapter.java
@@ -27,116 +27,102 @@ import static com.google.common.collect.Lists.newArrayList;
 /**
  * Created by ags on 05/07/12 at 21:06
  */
-public class SessionAdapter extends ArrayAdapter
-{
-  private static final int[] regular_backgrounds = new int[] {R.drawable.session_list_odd, R.drawable.session_list_even};
-  private static final int[] marked_backgrounds = new int[] {R.drawable.session_list_marked_odd, R.drawable.session_list_marked_even};
+public class SessionAdapter extends ArrayAdapter {
+    private static final int[] regular_backgrounds = new int[]{R.drawable.session_list_odd, R.drawable.session_list_even};
 
-  private ResourceHelper resourceHelper;
-  private Context context;
-  private SessionState sessionState;
+    private ResourceHelper resourceHelper;
+    private Context context;
+    private SessionState sessionState;
 
-  List<Session> sessions = newArrayList();
+    List<Session> sessions = newArrayList();
 
-  public SessionAdapter(Context context, SessionState sessionState) {
-    super(context, R.layout.session_row, R.id.session_title);
+    public SessionAdapter(Context context, SessionState sessionState) {
+        super(context, R.layout.session_row, R.id.session_title);
 
-    this.context = context;
-    this.sessionState = sessionState;
-  }
-
-  @Override
-  public View getView(int position, View convertView, ViewGroup parent)
-  {
-    View view = super.getView(position, convertView, parent);
-
-    Session session = sessions.get(position);
-
-    Drawable background = getBackground(position, session.getId());
-    view.setBackgroundDrawable(background);
-
-    fillTitle(view, context, session);
-    fillIcons(view, session);
-    fillStats(view, session);
-    fillTypes(view, session);
-
-    return view;
-  }
-
-  private void fillIcons(View view, Session session) {
-    ImageView imageView = (ImageView) view.findViewById(R.id.session_icon);
-    imageView.setImageResource(session.getDrawable());
-  }
-
-  private void fillStats(View view, Session session) {
-    ((TextView) view.findViewById(R.id.session_time)).setText(FormatHelper.timeText(session));
-
-    view.findViewById(R.id.avg_container).setVisibility(View.GONE);
-    view.findViewById(R.id.peak_container).setVisibility(View.GONE);
-  }
-
-  private Drawable getBackground(int position, Long sessionId) {
-    int id;
-
-    if (sessionState.isSessionBeingViewed(sessionId)) {
-      id = marked_backgrounds[position % marked_backgrounds.length];
-    } else {
-      id = regular_backgrounds[position % regular_backgrounds.length];
+        this.context = context;
+        this.sessionState = sessionState;
     }
 
-    return context.getResources().getDrawable(id);
-  }
+    @Override
+    public View getView(int position, View convertView, ViewGroup parent) {
+        View view = super.getView(position, convertView, parent);
 
-  private void fillTitle(View view, Context context, Session session)
-  {
-    TextView sessionTitle = (TextView) view.findViewById(R.id.session_title);
-    if (session.getTitle() != null && session.getTitle().length() > 0)
-    {
-      sessionTitle.setText(session.getTitle());
+        Session session = sessions.get(position);
+
+        Drawable background = getBackground(position, session.getId());
+        view.setBackgroundDrawable(background);
+
+        fillTitle(view, context, session);
+        fillIcons(view, session);
+        fillStats(view, session);
+        fillTypes(view, session);
+
+        return view;
     }
-    else
-    {
-      String unnamed = context.getString(R.string.unnamed_session);
-      sessionTitle.setText(Html.fromHtml(unnamed));
+
+    private void fillIcons(View view, Session session) {
+        ImageView imageView = (ImageView) view.findViewById(R.id.session_icon);
+        imageView.setImageResource(session.getDrawable());
     }
-  }
 
-  private void fillTypes(View view, Session session)
-  {
-    TextView dataTypes = (TextView) view.findViewById(R.id.data_types);
+    private void fillStats(View view, Session session) {
+        ((TextView) view.findViewById(R.id.session_time)).setText(FormatHelper.timeText(session));
 
-    Iterable<String> types = transform(session.getActiveMeasurementStreams(), new Function<MeasurementStream, String>()
-    {
-      @Override
-      public String apply(MeasurementStream input)
-      {
-        return input.getShortType();
-      }
-    });
-    types = Ordering.natural().sortedCopy(types);
-
-    String text = Joiner.on("/").join(types);
-    dataTypes.setText(text);
-  }
-
-  public void setResourceHelper(ResourceHelper resourceHelper)
-  {
-    this.resourceHelper = resourceHelper;
-  }
-
-  public void setSessions(List<Session> sessions)
-  {
-    this.sessions = sessions;
-    super.clear();
-    for (Session session : sessions)
-    {
-      super.add(session);
+        view.findViewById(R.id.avg_container).setVisibility(View.GONE);
+        view.findViewById(R.id.peak_container).setVisibility(View.GONE);
     }
-    notifyDataSetChanged();
-  }
 
-  public Session getSession(int position)
-  {
-    return sessions.get(position);
-  }
+    private Drawable getBackground(int position, Long sessionId) {
+        int id;
+
+        if (sessionState.isSessionBeingViewed(sessionId)) {
+            id = R.drawable.session_list_marked_even;
+        } else {
+            id = regular_backgrounds[position % regular_backgrounds.length];
+        }
+
+        return context.getResources().getDrawable(id);
+    }
+
+    private void fillTitle(View view, Context context, Session session) {
+        TextView sessionTitle = (TextView) view.findViewById(R.id.session_title);
+        if (session.getTitle() != null && session.getTitle().length() > 0) {
+            sessionTitle.setText(session.getTitle());
+        } else {
+            String unnamed = context.getString(R.string.unnamed_session);
+            sessionTitle.setText(Html.fromHtml(unnamed));
+        }
+    }
+
+    private void fillTypes(View view, Session session) {
+        TextView dataTypes = (TextView) view.findViewById(R.id.data_types);
+
+        Iterable<String> types = transform(session.getActiveMeasurementStreams(), new Function<MeasurementStream, String>() {
+            @Override
+            public String apply(MeasurementStream input) {
+                return input.getShortType();
+            }
+        });
+        types = Ordering.natural().sortedCopy(types);
+
+        String text = Joiner.on("/").join(types);
+        dataTypes.setText(text);
+    }
+
+    public void setResourceHelper(ResourceHelper resourceHelper) {
+        this.resourceHelper = resourceHelper;
+    }
+
+    public void setSessions(List<Session> sessions) {
+        this.sessions = sessions;
+        super.clear();
+        for (Session session : sessions) {
+            super.add(session);
+        }
+        notifyDataSetChanged();
+    }
+
+    public Session getSession(int position) {
+        return sessions.get(position);
+    }
 }
