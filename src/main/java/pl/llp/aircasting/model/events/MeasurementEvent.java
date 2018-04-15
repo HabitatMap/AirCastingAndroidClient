780b20c35362e08113c7b1983775f01bd3f6d2eb
Radosław Wieczorek <radoslaw.wieczorek@lunarlogic.io>
2018-04-15 13:16:09 +0200

    Fix formatting

--- src/main/java/pl/llp/aircasting/model/events/MeasurementEvent.java
+++ src/main/java/pl/llp/aircasting/model/events/MeasurementEvent.java
@@ -3,24 +3,20 @@ package pl.llp.aircasting.model.events;
 import pl.llp.aircasting.model.Measurement;
 import pl.llp.aircasting.model.Sensor;
 
-public class MeasurementEvent
-{
-  private Measurement measurement;
-  private Sensor sensor;
+public class MeasurementEvent {
+    private Measurement measurement;
+    private Sensor sensor;
 
-  public MeasurementEvent(Measurement measurement, Sensor sensor)
-  {
-    this.measurement = measurement;
-    this.sensor = sensor;
-  }
+    public MeasurementEvent(Measurement measurement, Sensor sensor) {
+        this.measurement = measurement;
+        this.sensor = sensor;
+    }
 
-  public Measurement getMeasurement()
-  {
-    return measurement;
-  }
+    public Measurement getMeasurement() {
+        return measurement;
+    }
 
-  public Sensor getSensor()
-  {
-    return sensor;
-  }
+    public Sensor getSensor() {
+        return sensor;
+    }
 }
