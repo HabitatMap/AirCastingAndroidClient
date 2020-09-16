36663d0d7a5f7725f547c9ac004fde0d6fcbf030
Maria Turnau-Gurgul <maria.turnau@lunarlogic.io>
2020-09-16 12:57:07 +0200

    Separated events: setting sensor and changing sensor in map activity. Fixed opening map on Stream View selection.

---
+++ src/main/java/pl/llp/aircasting/event/ui/SensorChangedEvent.java
@@ -0,0 +1,22 @@
+package pl.llp.aircasting.event.ui;
+
+import pl.llp.aircasting.model.Sensor;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: marcin
+ * Date: 14/11/13
+ * Time: 12:30
+ * To change this template use File | Settings | File Templates.
+ */
+public class SensorChangedEvent {
+    private Sensor sensor;
+
+    public SensorChangedEvent(Sensor sensor) {
+        this.sensor = sensor;
+    }
+
+    public Sensor getSensor() {
+        return sensor;
+    }
+}
